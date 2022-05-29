eb6db44dcee1420db93ad644ba205728ee9338d3
Francesco Schembri <francesco.schembri@studenti.unimi.it>
2022-05-29 18:58:06 +0200

    Findglm.cmake added

---
+++ installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install xorg-dev
+sudo apt-get install make
+sudo apt-get install cmake
+sudo apt-get install build-essential
+cd ~/Desktop
+git clone --recursive https://github.com/francescoschembri/TesiReskinner
+cd TesiReskinner/build
+cmake -S .. -B .
+make
